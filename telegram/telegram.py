263ad3102540734bac40f62ac1eb5cc9f27ab4c8
ilyasProgrammer <rakhimkulov@it-projects.info>
2016-07-12 15:15:43 +0500

    [IMP] basic mechanism of command. TODO telegram_listener need fix to access env

--- telegram/telegram.py
+++ telegram/telegram.py
@@ -1,16 +1,19 @@
 # -*- coding:utf-8 -*-
 
+import openerp
 from openerp import api, models, fields
 import openerp.addons.auth_signup.res_users as res_users
 from openerp.http import request
 from openerp import SUPERUSER_ID
-import openerp
+from openerp.addons.base.ir import ir_qweb
 from openerp.exceptions import ValidationError
-import logging
 from openerp.tools.safe_eval import safe_eval
 import datetime
 import dateutil
 import time
+import logging
+from lxml import etree
+from openerp.addons.base.ir.ir_qweb import QWebContext
 
 _logger = logging.getLogger('# Telegram')
 
@@ -20,44 +23,67 @@ SAFE_EVAL_BASE = {
     'time': time,
 }
 
+
 class TelegramCommand(models.Model):
     """
-        Model represents some command that may be proceeded.
+        Model represents Telegram commands that may be proceeded.
+        Other modules can add new commands by adding some records of telegram.command model.
+        Command must have:
+          - python_code to execute;
+          - response_code to handle odoo response on executed python_code as optional;
+          - and web controllers if it is needed.
     """
     _name = "telegram.command"
 
     name = fields.Char()
     python_code = fields.Char()
+    response_code = fields.Char()
     groups = fields.Char()
+    response_template = fields.Char()
+    notify_template = fields.Char()
 
     @api.model
     def telegram_listener(self, messages, bot):
+        # python_code execution method
         for m in messages:
             res = self.env['telegram.command'].search([('name', '=', m.text)], limit=1)
-            _logger.warning(res)
             if len(res) == 1:
-                safe_eval(res[0].python_code, SAFE_EVAL_BASE, {'self': self,
-                                                               'TelegramUser': TelegramUser,
-                                                               'bot': bot,
-                                                               'get_parameter': get_parameter,
-                                                               'm': m}, mode="exec", nocopy=True)
-            # if m.content_type == 'text':
-            #     if m.text == '/users':
-            #         TelegramUser.check_access(self.env, m.chat.id, '/users')
-            #         users_logintime_list = [str(r.name) + ', last login at: ' + str(r.login_date) for r in
-            #                                 self.env['res.users'].search([('name', '!=', None)])]
-            #         [bot.send_message(m.chat.id, r) for r in users_logintime_list]
-            #     elif m.text == '/mails':
-            #         pass
-            #     else:
-            #         bot.send_message(m.chat.id, 'You say ' + m.text)
+                locals_dict = {'self': self, 'bot': bot, 'm': m,
+                               'TelegramUser': TelegramUser,
+                               'get_parameter': get_parameter}
+                safe_eval(res[0].python_code, SAFE_EVAL_BASE, locals_dict, mode="exec", nocopy=True)
+                self.notify(bot, m, res[0].response_template, locals_dict)
+            elif len(res) > 1:
+                raise ValidationError('Multiple values for %s' % res)
+            else:
+                bot.send_message(m.chat.id, 'No such command: < %s > .' % m.text)
 
     def odoo_listener(self, message, bot):
-        # TODO exceptions ?
         m = message['message']
-        if m['action'] == 'login':
-            bot.send_message(m['chat_id'], 'Hello %s !' % m['odoo_user_name'])
-            #если тут возникает ошибка то она даже в логе не отображается
+        print '# 111:'
+        res = self.pool['telegram.command'].search(self._cr, self._uid, [('name', '=', m.text)], limit=1)
+        print '# 222:'
+        if len(res) == 1:
+            if res[0].response_code:
+                locals_dict = {'self': self, 'bot': bot, 'm': m,
+                               'TelegramUser': TelegramUser,
+                               'get_parameter': get_parameter}
+                safe_eval(res[0].response_code, SAFE_EVAL_BASE, locals_dict, mode="exec", nocopy=True)
+                self.notify(bot, m, res[0].notify_template, locals_dict)
+            else:
+                pass  # No response code for this command. Response code is optional.
+        elif len(res) > 1:
+            raise ValidationError('Multiple values for %s' % res)
+
+    def notify(self, bot, m, template, locals_dict):
+        """Response or notify user"""
+        qweb = self.pool['ir.qweb']
+        context = QWebContext(self._cr, self._uid, {})
+        ctx = context.copy()
+        ctx.update({'locals_dict': locals_dict})
+        dom = etree.fromstring(template)
+        rend = qweb.render_node(dom, ctx)
+        bot.send_message(m.chat.id, rend)
 
 
 class TelegramUser(models.TransientModel):
@@ -87,6 +113,8 @@ class TelegramUser(models.TransientModel):
         # tele_user_id = tele_env['telegram.user'].search([('chat_id', '=', chat_id)])
         # tele_user_obj = tele_env['telegram.user'].browse(tele_user_id)
         # TODO
+
+
 # query = """SELECT *
 #            FROM mail_message as a, mail_message_res_partner_rel as b
 #            WHERE a.id = b.mail_message_id
@@ -112,8 +140,9 @@ def get_parameter(db_name, key):
 
 
 def dump(obj):
-  for attr in dir(obj):
-    print "obj.%s = %s" % (attr, getattr(obj, attr))
+    for attr in dir(obj):
+        print "obj.%s = %s" % (attr, getattr(obj, attr))
+
 
 def dumpclean(obj):
     if type(obj) == dict:
