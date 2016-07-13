2617e5aca7c9127a77c1872de7fc8ccdee286d64
ilyasProgrammer <rakhimkulov@it-projects.info>
2016-07-13 13:30:41 +0500

    [IMP] Basic cache mechanics. Commands permission.

--- telegram/__init__.py
+++ telegram/__init__.py
@@ -1,12 +1,17 @@
 # -*- encoding: utf-8 -*-
+
+from openerp import models
 import telegram
 import telegram_bus
 import controllers
 import random
-
+import datetime
+import dateutil
+import time
 import openerp
 from openerp.service.server import Worker
 from openerp.service.server import PreforkServer
+from openerp.tools.safe_eval import safe_eval
 import telebot
 from telebot import TeleBot
 import telebot.util as util
@@ -19,6 +24,43 @@ import time
 from telebot import apihelper, types, util
 
 _logger = logging.getLogger('# Telegram')
+
+
+def get_parameter(db_name, key):
+    db = openerp.sql_db.db_connect(db_name)
+    registry = openerp.registry(db_name)
+    result = None
+    with openerp.api.Environment.manage(), db.cursor() as cr:
+        res = registry['ir.config_parameter'].search(cr, SUPERUSER_ID, [('key', '=', key)])
+        if len(res) == 1:
+            val = registry['ir.config_parameter'].browse(cr, SUPERUSER_ID, res[0])
+            result = val.value
+        elif len(res) > 1:
+            raise ValidationError('Multiple values for %s' % key)
+        elif len(res) < 1:
+            _logger.debug("WARNING. No value for key %s" % key)
+            return None
+    return result
+def running_workers_num(workers):
+    res = 0
+    for r in workers:
+        if r._running:
+            res += 1
+    return res
+def _db_list():
+    if config['db_name']:
+        db_names = config['db_name'].split(',')
+    else:
+        db_names = openerp.service.db.list_dbs(True)
+    return db_names
+
+globals_dict = {
+    'datetime': datetime,
+    'dateutil': dateutil,
+    'time': time,
+    'get_parameter': get_parameter,
+    '_logger': _logger,
+}
 
 
 def telegram_worker():
@@ -248,40 +290,20 @@ class TeleBotMod(TeleBot, object):
     def __init__(self, token, threaded=True, skip_pending=False, num_threads=2):
         super(TeleBotMod, self).__init__(token, threaded=False, skip_pending=skip_pending)
         self.worker_pool = util.ThreadPool(num_threads)
+        self.cache = CommandCache()
         _logger.info("TeleBot started with %s threads" % num_threads)
 
 
-def get_parameter(db_name, key):
-    db = openerp.sql_db.db_connect(db_name)
-    registry = openerp.registry(db_name)
-    result = None
-    with openerp.api.Environment.manage(), db.cursor() as cr:
-        res = registry['ir.config_parameter'].search(cr, SUPERUSER_ID, [('key', '=', key)])
-        if len(res) == 1:
-            val = registry['ir.config_parameter'].browse(cr, SUPERUSER_ID, res[0])
-            result = val.value
-        elif len(res) > 1:
-            raise ValidationError('Multiple values for %s' % key)
-        elif len(res) < 1:
-            _logger.debug("WARNING. No value for key %s" % key)
-            return None
-    return result
+class CommandCache(object):
+    def __init__(self):
+        self.vals = {}           # vals{command_id(users_results{}, result)}
+        # self.command_id = ()
+        # self.users_results = {}  # user - answer dict. Answer for every allowed user.
+        # self.result = ''         # Answer for all users.
 
+    def update(self, command_id, result, users_results=False):
+        self.vals.update({command_id: {'result': result, 'users_results': users_results}})
 
-def running_workers_num(workers):
-    res = 0
-    for r in workers:
-        if r._running:
-            res += 1
-    return res
-
-
-def _db_list():
-    if config['db_name']:
-        db_names = config['db_name'].split(',')
-    else:
-        db_names = openerp.service.db.list_dbs(True)
-    return db_names
 
 
 def dump(obj):
--- telegram/telegram.py
+++ telegram/telegram.py
@@ -18,127 +18,8 @@ from lxml import etree
 from openerp.addons.base.ir.ir_qweb import QWebContext
 
 _logger = logging.getLogger('# Telegram')
+_logger.setLevel(logging.DEBUG)
 # telebot.logger.setLevel(logging.DEBUG)
-SAFE_EVAL_BASE = {
-    'datetime': datetime,
-    'dateutil': dateutil,
-    'time': time,
-}
-
-
-class TelegramCommand(models.Model):
-    """
-        Model represents Telegram commands that may be proceeded.
-        Other modules can add new commands by adding some records of telegram.command model.
-        Command must have:
-          - python_code to execute;
-          - response_code to handle odoo response on executed python_code as optional;
-          - and web controllers if it is needed.
-    """
-    _name = "telegram.command"
-
-    name = fields.Char()
-    python_code = fields.Char()
-    response_code = fields.Char()
-    group_ids = fields.One2many('res.groups', 'telegram_command_id')
-    response_template = fields.Char()
-    notify_template = fields.Char()
-
-    @api.model
-    def telegram_listener(self, messages, bot):
-        # python_code execution method
-        for m in messages:  # messages from telegram server
-            res = self.env['telegram.command'].search([('name', '=', m.text)], limit=1)
-            if len(res) == 1:
-                locals_dict = {'self': self, 'bot': bot, 'm': m,
-                               'TelegramUser': TelegramUser,
-                               'get_parameter': get_parameter}
-                safe_eval(res[0].python_code, SAFE_EVAL_BASE, locals_dict, mode="exec", nocopy=True)
-                self.render_and_send(bot, res[0].response_template, locals_dict, telegram_message=m)
-            elif len(res) > 1:
-                raise ValidationError('Multiple values for %s' % res)
-            else:
-                bot.send_message(m.chat.id, 'No such command: < %s > .' % m.text)
-
-    @api.model
-    def odoo_listener(self, message, bot):
-        m = message['message']  # message from bus, not from telegram server.
-        registry = openerp.registry(bot.db_name)
-        db = openerp.sql_db.db_connect(bot.db_name)
-        with openerp.api.Environment.manage(), db.cursor() as cr:
-            command_id = registry['telegram.command'].search(cr, SUPERUSER_ID, [('name', '=', m['action'])])
-            command = registry['telegram.command'].browse(cr, SUPERUSER_ID, command_id)
-            if len(command) == 1:
-                if command.response_code:
-                    locals_dict = {'bot': bot, 'm': m,
-                                   'TelegramUser': TelegramUser,
-                                   'get_parameter': get_parameter}
-                    safe_eval(command.response_code, SAFE_EVAL_BASE, locals_dict, mode="exec", nocopy=True)
-                    self.render_and_send(bot, command.notify_template, locals_dict, bus_message=m)
-                else:
-                    pass  # No response code for this command. Response code is optional.
-            elif len(command) > 1:
-                raise ValidationError('Multiple values for %s' % command)
-
-    def render_and_send(self, bot, template, locals_dict, bus_message=False, telegram_message=False):
-        """Response or notify user. template - xml to render with locals_dict."""
-        qweb = self.pool['ir.qweb']
-        context = QWebContext(self._cr, self._uid, {})
-        ctx = context.copy()
-        ctx.update({'locals_dict': locals_dict})
-        dom = etree.fromstring(template)
-        rend = qweb.render_node(dom, ctx)
-        _logger.info(rend)
-        if bus_message:
-            chat_id = bus_message['chat_id']
-        elif telegram_message:
-            chat_id = telegram_message.chat.id
-        else:
-            return
-        bot.send_message(chat_id, rend, parse_mode='HTML')
-
-
-class TelegramUser(models.TransientModel):
-    _name = "telegram.user"
-
-    chat_id = fields.Char()  # Primary key
-    token = fields.Char()
-    logged_in = fields.Boolean()
-    res_user = fields.Many2one('res.users')  # Primary key
-
-    @staticmethod
-    def register_user(tele_env, chat_id):
-        tele_user_id = tele_env['telegram.user'].search([('chat_id', '=', chat_id)])
-        if len(tele_user_id) == 0:
-            login_token = res_users.random_token()
-            vals = {'chat_id': chat_id, 'token': login_token}
-            new_tele_user = tele_env['telegram.user'].create(vals)
-        else:
-            tele_user_obj = tele_env['telegram.user'].browse(tele_user_id.id)
-            login_token = tele_user_obj.token  # user already exists
-
-        return login_token
-
-    @staticmethod
-    def check_access(tele_env, chat_id, command):
-        pass
-        # tele_user_id = tele_env['telegram.user'].search([('chat_id', '=', chat_id)])
-        # tele_user_obj = tele_env['telegram.user'].browse(tele_user_id)
-        # TODO
-
-
-class ResGroups(models.Model):
-    _inherit = 'res.groups'
-
-    telegram_command_id = fields.Many2one('telegram.command')
-
-# query = """SELECT *
-#            FROM mail_message as a, mail_message_res_partner_rel as b
-#            WHERE a.id = b.mail_message_id
-#            AND b.res_partner_id = %s""" % (5,)
-# self.env.cr.execute(query)
-# query_results = self.env.cr.dictfetchall()
-#
 
 
 def get_parameter(db_name, key):
@@ -155,6 +36,216 @@ def get_parameter(db_name, key):
             return None
     return result
 
+
+globals_dict = {
+    'datetime': datetime,
+    'dateutil': dateutil,
+    'time': time,
+    'get_parameter': get_parameter,
+    '_logger': _logger,
+}
+
+
+class TelegramCommand(models.Model):
+    """
+        Model represents Telegram commands that may be proceeded.
+        Other modules can add new commands by adding some records of telegram.command model.
+        Short commands gives result right after response_code is done.
+        Long commands gives result after job is done, when appropriate notification appears in bus.
+
+        Members:
+          response_code - python code to execute task. Launched by telegram_listener
+          response_template - Template of message, that user will receive immediately after he send command
+          notify_code - python code to get data, computed after executed action code. Launched by odoo_listener (bus)
+          notify_template - Template of message, that user will receive after job is done
+          update_cache_code - python code to update cache. Launched by ir.actions.server
+          group_ids - Who can use this command
+          model_ids - These models changes initiates cache updates for this command.
+
+    """
+    _name = "telegram.command"
+
+    name = fields.Char()
+    description = fields.Char()
+    cacheable = fields.Boolean()
+    response_code = fields.Char()
+    response_template = fields.Char()
+    notify_code = fields.Char()
+    notify_template = fields.Char()
+    group_ids = fields.One2many('res.groups', 'telegram_command_id')
+    model_ids = fields.Many2many('ir.model', 'command_to_model_rel', 'command_id', 'model_id')
+
+    @api.model
+    def telegram_listener(self, messages, bot):
+        # python_code execution method
+        for tele_message in messages:  # messages from telegram server
+            res = self.env['telegram.command'].search([('name', '=', tele_message.text)], limit=1)
+            command = res[0] if len(res) > 0 else False
+            if len(res) == 1:
+                tele_user = self.env['telegram.user'].search([('chat_id', '=', tele_message.chat.id)])
+                if command.name not in ['/login', '/help']:
+                    if not tele_user.logged_in:
+                        bot.send_message(tele_message.chat.id, 'You have to /login first.')
+                        return
+                    if not self.access_granted(command, tele_message.chat.id):
+                        bot.send_message(tele_message.chat.id, 'Access denied. Command:  %s  .' % tele_message.text)
+                        return
+                if command.name == '/login' and tele_user.logged_in:
+                    bot.send_message(tele_message.chat.id, 'You already logged in.')
+                    return
+                locals_dict = {'env': self.env, 'bot': bot, 'tele_message': tele_message, 'TelegramUser': TelegramUser}
+                need_computed_answer = True
+                if command.id in bot.cache.vals:
+                    command_cache = bot.cache.vals[command.id]
+                    _logger.debug('got cache for this command')
+                    _logger.debug(command_cache)
+                    if command_cache['result']:
+                        locals_dict.update(command_cache['result'])
+                        self.render_and_send(bot, tele_message.chat.id, command.response_template, locals_dict, tele_message=tele_message)
+                        need_computed_answer = False
+                        _logger.debug('Sent answer from cache')
+                    elif len(command_cache['users_results']):
+                        for usr_cache_line in command_cache['users_results']:
+                            locals_dict.update(usr_cache_line['result'])
+                            tele_user = self.env['telegram.user'].search([('id', '=', usr_cache_line['user_id']),
+                                                                          ('chat_id', '=', tele_message.chat.id)])
+                            if len(tele_user) > 0:
+                                self.render_and_send(bot, tele_message.chat.id, command.response_template, locals_dict, tele_message=tele_message)
+                            need_computed_answer = False
+                if need_computed_answer:
+                    _logger.debug('No cache. Computing answer ...')
+                    safe_eval(command.response_code, globals_dict, locals_dict, mode="exec", nocopy=True)
+                    self.render_and_send(bot, tele_message.chat.id, command.response_template, locals_dict, tele_message=tele_message)
+            elif len(res) > 1:
+                raise ValidationError('Multiple values for %s' % res)
+            else:
+                bot.send_message(tele_message.chat.id, 'No such command:  %s  . Use /help to see all commands.' % tele_message.text)
+
+    @api.model
+    def odoo_listener(self, message, bot):
+        bus_message = message['message']  # message from bus, not from telegram server.
+        registry = openerp.registry(bot.db_name)
+        db = openerp.sql_db.db_connect(bot.db_name)
+        with openerp.api.Environment.manage(), db.cursor() as cr:
+            _logger.debug('bus_message')
+            _logger.debug(bus_message)
+            if bus_message['action'] == 'update_cache':
+                self.update_cache(bus_message, bot)
+            else:
+                command_id = registry['telegram.command'].search(cr, SUPERUSER_ID, [('name', '=', bus_message['action'])])
+                command = registry['telegram.command'].browse(cr, SUPERUSER_ID, command_id)
+                if len(command) == 1:
+                    if command.notify_code:
+                        locals_dict = {'bot': bot, 'bus_message': bus_message, 'TelegramUser': TelegramUser}
+                        safe_eval(command.notify_code, globals_dict, locals_dict, mode="exec", nocopy=True)
+                        _logger.debug('locals_dict')
+                        _logger.debug(locals_dict)
+                        self.render_and_send(bot, bus_message['chat_id'], command.notify_template, locals_dict, bus_message=bus_message)
+                    else:
+                        pass  # No notify_code for this command. Response code is optional.
+                elif len(command) > 1:
+                    raise ValidationError('Multiple values for %s' % command)
+
+    def render_and_send(self, bot, chat_id, template, locals_dict, bus_message=False, tele_message=False):
+        """Response or notify user. template - xml to render with locals_dict."""
+        qweb = self.pool['ir.qweb']
+        context = QWebContext(self._cr, self._uid, {})
+        ctx = context.copy()
+        ctx.update({'locals_dict': locals_dict['result']})
+        dom = etree.fromstring(template)
+        rend = qweb.render_node(dom, ctx)
+        _logger.debug('render_and_send(): ' + rend)
+        if bus_message:
+            chat_id = bus_message['chat_id']
+        elif tele_message:
+            chat_id = tele_message.chat.id
+        else:
+            return
+        bot.send_message(chat_id, rend, parse_mode='HTML')
+
+    def update_cache_bus_message(self, cr, uid, ids, context):
+        # Called by run_telegram_commands_cache_updates (ir.actions.server)
+        _logger.debug('update_cache_bus_message(): start')
+        found_commands_ids = self.pool['telegram.command'].search(cr, uid, [('model_ids.model', '=', context['active_model']), ('cacheable', '=', True)])
+        if len(found_commands_ids):
+            _logger.debug('update_cache_bus_message(): commands will got cache update:')
+            _logger.debug(found_commands_ids)
+            message = {'update_cache': True, 'model': context['active_model'], 'found_commands_ids': found_commands_ids}
+            self.pool['telegram.bus'].sendone(cr, SUPERUSER_ID, 'telegram_channel', message)
+
+    def update_cache(self, bus_message, bot):
+        _logger.debug('update_cache() - command from bus')
+        for command_id in bus_message['found_commands_ids']:
+            command = self.env['telegram.command'].browse(command_id)
+            locals_dict = {'bot': bot, 'env': self.env,'bus_message': bus_message, 'TelegramUser': TelegramUser}
+            if len(command.group_ids):
+                all_users_result = False
+                users_results = {}
+                users = self.env['res.user'].search([('groups_ids', 'in', command.group_ids)])
+                for user in users:
+                    locals_dict.update({'user_id': user.id})
+                    safe_eval(command.response_code, globals_dict, locals_dict, mode="exec", nocopy=True)
+                    users_results.update({'user_id': user.id, 'result': locals_dict['result']})
+            else:
+                users_results = False
+                safe_eval(command.response_code, globals_dict, locals_dict, mode="exec", nocopy=True)
+                all_users_result = locals_dict['result']
+            bot.cache.update(command.id, all_users_result, users_results)
+
+    def access_granted(self, command, chat_id):
+        # granted or not ?
+        command_groups = set(self.env['res.groups'].search([('telegram_command_id', '=', command.id)]))
+        if len(command_groups) == 0:
+            return True  # If no groups than every one can use this command
+        tele_user = self.env['telegram.user'].search([('chat_id', '=', chat_id)])
+        user_groups = set(tele_user.res_user.groups_id)
+        if len(command_groups.intersection(user_groups)):
+            return True
+        return False
+
+
+class TelegramUser(models.TransientModel):
+    _name = "telegram.user"
+
+    chat_id = fields.Char()  # Primary key
+    token = fields.Char()
+    logged_in = fields.Boolean()
+    res_user = fields.Many2one('res.users')  # Primary key
+
+    @staticmethod
+    def register_user(tele_env, chat_id):
+        tele_user_id = tele_env['telegram.user'].search([('chat_id', '=', chat_id)])
+        if len(tele_user_id) == 0:
+            login_token = res_users.random_token()
+            vals = {'chat_id': chat_id, 'token': login_token, 'logged_in': False}
+            new_tele_user = tele_env['telegram.user'].create(vals)
+        else:
+            tele_user_obj = tele_env['telegram.user'].browse(tele_user_id.id)
+            login_token = tele_user_obj.token  # user already exists
+
+        return login_token
+
+    @staticmethod
+    def logout(env, chat_id):
+        tele_user_id = env['telegram.user'].search([('chat_id', '=', chat_id)])
+        tele_user_id.logged_in = False
+
+
+
+class ResGroups(models.Model):
+    _inherit = 'res.groups'
+
+    telegram_command_id = fields.Many2one('telegram.command')
+
+
+
+    # query = """SELECT *
+#            FROM mail_message as a, mail_message_res_partner_rel as b
+#            WHERE a.id = b.mail_message_id
+#            AND b.res_partner_id = %s""" % (5,)
+# self.env.cr.execute(query)
+# query_results = self.env.cr.dictfetchall()
+#
 
 def dump(obj):
     for attr in dir(obj):
